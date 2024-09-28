e68c9233d41c6b9fc0410d485e2bbdfcd46765dd
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-09-29 04:41:11 +0700

    feat: a

--- SoftDrunk/Controllers/HomeController.cs
+++ SoftDrunk/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SoftDrunk.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page. Oggy";
 
             return View();
         }
